78d6e3d1070dc95290a0373fcd816f5f509d685f
Belliah-PRo <83418149+Belliah-PRo@users.noreply.github.com>
2022-10-22 19:02:59 +0100

    top button finished

---
+++ button.js
@@ -0,0 +1,27 @@
+const button = document.querySelector('.button');
+
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+  
+    if (window.scrollY > 100) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    }); 
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
